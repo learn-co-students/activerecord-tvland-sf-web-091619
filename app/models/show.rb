2507a6d11364a26408829b6189f919a8bc11b46b
github username <github email address>
2019-09-27 14:19:56 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        array = []
+        fn = self.actors.map do |t|
+            t.first_name
+        end
+        ln = self.actors.map do |t|
+            t.last_name
+        end
+        name = [fn, ln].join(" ")
+        array << name
+    end
+
   
 end
